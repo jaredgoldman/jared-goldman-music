04f02f185c06514467859a342ea1158ee8d99faf
jaredgoldman <jaredgoldmandev@gmail.com>
2023-03-20 20:22:22 -0400

    Style form, add responsive styling on index and gig page

--- scripts/sharedComponents.js
+++ scripts/sharedComponents.js
@@ -1,24 +1,35 @@
 class Header extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
-            <div class="header-links_container">
+            <nav class="header-links_container">
                 <div class="header-links">
-                    <div>
+                    <div class="header-link_container">
                         <a class="header-link" href="/">JG</a>
                     </div>
-                    <div>
+                    <div class="header-link_container">
                         <a class="header-link" href="/gigs.html">Gigs</a>
                     </div>
-                    <div>
+                    <div class="header-link_container">
                         <a class="header-link" href="/media.html">Media</a>
                     </div>
-                    <div>
+                    <div class="header-link_container">
                         <a class="header-link" href="/music.html">Music</a>
                     </div>
                 </div>
-            </div>
+            </nav>
     `
     }
 }
 
+class Footer extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+            <footer>
+               Made with love by JG
+            </footer>
+        `
+    }
+}
+
 customElements.define('main-header', Header)
+customElements.define('main-footer', Footer)
