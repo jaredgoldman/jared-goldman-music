65da60313c554a1b31cbd95e65c1b6a6be5ee2fb
jaredgoldman <jaredgoldmandev@gmail.com>
2023-03-27 23:25:04 -0400

    Add success badge

--- scripts/contact.js
+++ scripts/contact.js
@@ -5,6 +5,7 @@ const submitContactMessage = () => {
     const nameInput = document.getElementById('contact-name')
     const emailInput = document.getElementById('contact-email')
     const messageInput = document.getElementById('contact-message')
+    const successBadge = document.querySelector('.home-contact-form_success')
     form.addEventListener('submit', async (event) => {
         event.preventDefault()
         const name = nameInput.value
@@ -12,8 +13,10 @@ const submitContactMessage = () => {
         const message = messageInput.value
         console.log(`contact info: ${name}, ${email}, ${message}`)
         const res = await request('/contact', 'POST', { name, email, message })
-        if (res.accepted[0] === email) {
-            // display success badge
+        console.log(res)
+        if (res.accepted.length) {
+            console.log('message sent')
+            successBadge.classList.add('home-contact-form_success-active')
         } else {
             // display error badge
         }
