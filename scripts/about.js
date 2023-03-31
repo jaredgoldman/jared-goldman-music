0af5f484e48845d916bbccd244d5a0c0bd2225f0
jaredgoldman <jaredgoldmandev@gmail.com>
2023-03-31 16:35:30 -0400

    Add some colors and setup css variables for global styling

--- scripts/about.js
+++ scripts/about.js
@@ -1,7 +1,7 @@
 import { request } from "./shared.js";
 
 const loadContent = async () => {
-    const { data: { attributes: { bio } } } = await request('/about')
+    const { data: { attributes: { bio } } } = await request('/bio')
     const bioNode = document.querySelector('.bio')
     console.log(bioNode)
     const bioParagraph = document.createElement('p')
--- scripts/shared.js
+++ scripts/shared.js
@@ -1,8 +1,6 @@
 import { CMS_API_KEY, API_URL } from '../config.js'
 
 const request = async (path, method = 'GET', data = null) => {
-    console.log(CMS_API_KEY)
-    console.log(API_URL)
     const body = method !== 'GET' && data ? JSON.stringify(data) : null
     const res = await fetch(`${API_URL}${path}`, {
         headers: {
