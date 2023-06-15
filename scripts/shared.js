669baec27bef48a179bb1651a2018263b6494ae1
jaredgoldman <jaredgoldmandev@gmail.com>
2023-06-15 17:10:04 -0400

    Update TOKEN to KEY in config

--- scripts/shared.js
+++ scripts/shared.js
@@ -1,10 +1,10 @@
-import { API_KEY, API_URL } from '../config.js'
+import { API_TOKEN, API_URL } from '../config.js'
 
 const request = async (path, method = 'GET', data = null) => {
     const body = method !== 'GET' && data ? JSON.stringify(data) : null
     const res = await fetch(`${API_URL}/api${path}`, {
         headers: {
-            Authorization: `bearer ${API_KEY}`,
+            Authorization: `bearer ${API_TOKEN}`,
         },
         method,
         body,
