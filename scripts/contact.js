5e362ee5b62f3f5dc973db02ebc72b9a19bfe8ff
jaredgoldman <jaredgoldmandev@gmail.com>
2023-06-15 22:10:08 -0400

    FIX: Ensure error text is cleared

--- scripts/contact.js
+++ scripts/contact.js
@@ -6,19 +6,32 @@ const submitContactMessage = () => {
     const emailInput = document.getElementById('contact-email')
     const messageInput = document.getElementById('contact-message')
     const successBadge = document.querySelector('.home-contact-form_success')
+    const errorBadge = document.querySelector('.home-contact-form_error')
+    errorBadge.innerHTML = ''
+
     form.addEventListener('submit', async (event) => {
         event.preventDefault()
         const name = nameInput.value
         const email = emailInput.value
         const message = messageInput.value
         const referrer = 'music'
+
+        if (!name || !email || !message) {
+            // display error badge
+            errorBadge.innerHTML = 'Please fill out all fields'
+            setTimeout(() => {
+                errorBadge.innerHTML = ''
+            }, 3000)
+            return
+        }
+
         const res = await request('/contact', 'POST', {
             name,
             email,
             message,
             referrer,
         })
-        console.log(res)
+
         if (res.accepted.length) {
             successBadge.classList.add('home-contact-form_success-active')
             setTimeout(() => {
@@ -28,6 +41,7 @@ const submitContactMessage = () => {
             }, 3000)
         } else {
             // display error badge
+            errorBadge.innerHTML = 'Something went wrong'
         }
     })
 }
